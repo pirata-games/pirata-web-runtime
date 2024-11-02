0ee2119607dadf37a62e57bb677c2998f889806f
milehighjackal <milehighjackal@gmail.com>
2024-11-01 19:47:30 -0600

    Latest

--- apps/home/index.tsx
+++ apps/home/index.tsx
@@ -6,30 +6,33 @@ export const IsIsland = true;
 // deno-lint-ignore ban-types
 type IndexPageData = {};
 
-export const handler: EaCRuntimeHandlerResult<CompanyWebState, IndexPageData> = {
-  GET: (_req, ctx) => {
-    return ctx.Render({});
-  },
-};
+export const handler: EaCRuntimeHandlerResult<CompanyWebState, IndexPageData> =
+  {
+    GET: (_req, ctx) => {
+      return ctx.Render({});
+    },
+  };
 
 export default function Index({}: PageProps<IndexPageData>) {
   return (
     <div
-      className='relative w-screen h-screen bg-cover bg-center bg-no-repeat'
+      className="relative w-screen h-screen bg-cover bg-center bg-no-repeat"
       style={{
         backgroundImage: `url('/assets/hero-background.png')`,
       }}
     >
-      <button className='absolute top-5 right-5 bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-200'>
+      {/* Sign In Button */}
+      <button className="absolute top-5 right-5 bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-200 text-sm sm:text-base">
         Sign In
       </button>
 
-      <div className='flex items-start justify-center h-full'>
+      {/* Logo */}
+      <div className="flex items-start justify-center h-full">
         <img
-          src='/assets/PirataForsaken.png'
+          src="/assets/PirataForsaken.png"
           data-eac-bypass-base
-          alt='Pirata: Forsaken Logo'
-          className='w-1/3 max-w-md mt-6'
+          alt="Pirata: Forsaken Logo"
+          className="landscape:mt-1 md:mt-[5rem] sm:mt-[2rem] mt-1 md:w-[600px] sm:w-[350px] w-[250px]"
         />
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,11 @@ import unimportant from 'tailwindcss/unimportant';
 export default {
   content: [],
   plugins: [unimportant],
+  theme: {
+    extend: {
+      screens: {
+        'landscape': { 'raw': '(orientation: landscape)' },
+      },
+    },
+  },
 } satisfies Config;
