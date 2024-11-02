b66eca474056d59bf5758a9c48f1b2b1ecb73114
milehighjackal <milehighjackal@gmail.com>
2024-11-02 08:39:24 -0600

    updated deploy and favicon

--- apps/home/_layout.tsx
+++ apps/home/_layout.tsx
@@ -8,20 +8,26 @@ export default function Layout({
   return (
     <html>
       <head>
-        <meta charset='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <title>Fathym EaC Runtime</title>
 
-        <link rel='shortcut icon' type='image/png' href='/thinky.png' />
         <link
-          rel='stylesheet'
+          rel="shortcut icon"
+          type="image/png"
+          href="/assets/PirataForsaken.png"
+          data-eac-bypass-base
+        />
+
+        <link
+          rel="stylesheet"
           href={`/tailwind/styles.css?Revision=${Revision}`}
           data-eac-bypass-base
         />
       </head>
 
-      <body class='bg-slate-50 dark:bg-slate-900 text-black dark:text-white'>
+      <body class="bg-slate-50 dark:bg-slate-900 text-black dark:text-white">
         <Component />
       </body>
     </html>
