7ddf852123694e7bc6d8ee939cf5d14fb1dc5b6a
milehighjackal <milehighjackal@gmail.com>
2024-11-08 19:19:47 -0700

    remove test display

--- apps/adb2c/unified.tsx
+++ apps/adb2c/unified.tsx
@@ -1,6 +1,6 @@
 import { EaCRuntimeHandlerResult, PageProps } from '@fathym/eac-runtime';
 import { PirataGamesWebState } from '../../src/state/PirataGamesWebState.ts';
-import { ADB2CTestSignIn } from '../components/test-signin.tsx';
+// import { ADB2CTestSignIn } from '../components/test-signin.tsx';
 // import { ADB2CTestSignUp } from '../components/test-signup.tsx';
 // import { ADB2CTestPasswordReset } from '../components/test-resetpassword.tsx';
 
@@ -19,7 +19,7 @@ export const handler: EaCRuntimeHandlerResult<
 export default function ADB2CUnified({}: PageProps<ADB2CUnifiedPageData>) {
   return (
     <div id='api' role='main'>
-      <ADB2CTestSignIn />
+      {/* <ADB2CTestSignIn /> */}
       {/* <ADB2CTestSignUp /> */}
       {/* <ADB2CTestPasswordReset /> */}
     </div>
